72153359dcdc8eedbb14139c8586538f077fb074
bianjie1314 <814094126@qq.com>
2018-08-12 00:00:28 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 #编译+部署站点
+
 #需要配置如下参数
 #项目路径,在Execute_Shell中配置项目路径,pwd 就可以获得该项目路径
 #export PROJ_PATH=这个jenkins任务部署机器上的路径
+
 #输入你的环境tomccat的全路径
 #export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 ### base 函数
@@ -12,23 +14,29 @@ killTomcat()
 	echo "tomcat Id list:$pid"
 	if [ "$pid" = "" ]
 	then
-	  echo "no tomcat pid value"
+		echo "no tomcat pid value"
 	else
-	  kill -9 $pid
+		kill -9 $pid
 	fi
 }
-cd $PROJ_PATH/order
+
+cd $PROJ_PATH/test-jenkins
 mvn clean install
+
 #停止tomcat
 killTomcat
+
 #删除原有工程
 rm -rf $TOMCAT_APP_PATH/webapps/TOOR
 rm -f $TOMCAT_APP_PATH/webapps/TOOR.war
 rm -f $TOMCAT_APP_PATH/webapps/test-jenkins.war
+
 #复制新的工程
 cp $PROJ_PATH/test-jenkins/target/test-jenkins.war $TOMCAT_APP_PATH/webapps/
+
 cd $TOMCAT_APP_PATH/webapps/
 mv test-jenkins.war ROOT.war
+
 #启动tomcat
 cd $TOMCAT_APP_PATH
 sh bin/startup.sh
